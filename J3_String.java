21cf1e886710c540c1802e6873b27dcf2b2a557b
Ajay Mali <ajaymali2701@gmail.com>
2024-06-16 21:00:01 +0530

    Create J3_String.java

---
+++ J3_String.java
@@ -0,0 +1,10 @@
+public class J3_String {
+    public static void main(String[] args) {
+        String name = "Striver";
+        int len = name.length();
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(5 - 1));
+
+    }
+}
