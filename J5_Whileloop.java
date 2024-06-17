5851a08c233b2be91b003f1b2ae586336670e9e4
Ajay Mali <ajaymali2701@gmail.com>
2024-06-17 16:48:19 +0530

    While loop and do while loop

---
+++ J5_Whileloop.java
@@ -0,0 +1,27 @@
+public class J5_Whileloop {
+    public static void main(String[] args) {
+
+
+        // While Loop ;
+        
+        int i = 1;
+        while (i < 5) {
+
+        System.out.print(" Hello World ");
+        i++;
+        }
+
+
+
+
+        // Do while Loop;
+        int num = 10;
+
+        do {
+            System.out.println(num);
+            num++;
+        } while (num <= 5);
+
+    }
+
+}
