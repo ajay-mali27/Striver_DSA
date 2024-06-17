58dd8e2aad117335534d5c2fda58420af5023389
Ajay Mali <ajaymali2701@gmail.com>
2024-06-17 09:03:55 +0530

    Create J4_ForLoop.java

---
+++ J4_ForLoop.java
@@ -0,0 +1,15 @@
+/**
+ * ForLoop
+ */
+public class J4_ForLoop {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 5; i++) {
+            System.err.println(i);
+
+        }
+        System.out.println(i);
+    }
+
+}
